ccf0cab22c88046bc1386bd14aa4112fbcb2c167
Edagotti Kishore <kishorebytes101@gmail.com>
2026-06-06 01:19:40 +0530

    Setup user module foundation

--- src/main/java/org/example/cafemanagementsystem/entity/User.java
+++ src/main/java/org/example/cafemanagementsystem/entity/User.java
@@ -1,4 +1,36 @@
 package org.example.cafemanagementsystem.entity;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
