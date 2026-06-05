05bf33f1d4b41883558c260db8e8b1642ad62e74
Edagotti Kishore <kishorebytes101@gmail.com>
2026-06-05 23:43:34 +0530

    Initial Spring Boot Project setup

---
+++ src/test/java/org/example/cafemanagementsystem/CafeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cafemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
